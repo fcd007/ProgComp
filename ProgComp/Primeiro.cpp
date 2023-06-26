5caa8ac7860592c20ccdbd3971975f1b2eadc644
Francisco Claudeilton Dantas <fcd007@hotmail.com>
2023-06-26 20:41:21 -0300

    Add project files.

---
+++ ProgComp/Primeiro.cpp
@@ -0,0 +1,12 @@
+// Primeiro.cpp - mostrar uma mensagem
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+	cout << "Bem vindo ao C++";
+	cout << endl;
+	cout << "Meu primeiro programa!" << endl;
+
+	return 0;
+}
